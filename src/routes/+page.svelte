0c095ff94e6adeeeed9a5a2cfadefce66a5703a0
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-10-04 09:24:07 -0300

    (up) Issue #deploy_github_pages.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+	<title>isma.codes</title>
+	<meta name="description" content="Ismael Pamplona personal web site" />
+</svelte:head>
+
 <h1>Ismael Pamplona</h1>
 <h2>Welcome to SvelteKit</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
