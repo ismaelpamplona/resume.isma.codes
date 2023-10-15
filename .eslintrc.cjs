038fdd38599de792278ae9ada2a0c76c4f97cbed
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-10-15 07:26:04 -0300

    (up) Issue #general_app_config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,34 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'plugin:mdx/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
-};
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'plugin:mdx/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  rules: {
+    semi: ['error', 'never'],
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    },
+  ]
+}
+
--- ripi/Issue/general_app_config/description.md
+++ ripi/Issue/general_app_config/description.md
@@ -1,1 +1,12 @@
 # general_app_config (Issue)
+  - https://github.com/gubasso/gubasso.xyz
+
+## todo
+- [x] .editorconfig
+- [x] .pre-commit-config.yaml
+- [ ] .prettierrc
+  - [x] add svelte plugin
+  - [ ] add ts plugin
+- [x] .tool-versions
+- [ ] mdsvex.config.js
+- [x] setup
